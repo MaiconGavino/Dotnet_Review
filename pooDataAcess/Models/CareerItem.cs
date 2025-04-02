76bbeac05315677726549be8d4391e05b4231cdc
Maicon Gavino <maiconserrao57@gmail.com>
2025-04-02 16:40:32 -0300

    V1

---
+++ pooDataAcess/Models/CareerItem.cs
@@ -0,0 +1,11 @@
+using pooDataAccess.Models;
+
+namespace pooDataAcess.Models
+{
+    public class CareerItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ pooDataAcess/Models/Course.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace pooDataAccess.Models
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
